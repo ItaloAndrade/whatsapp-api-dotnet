c3c351d03f40df7fdeec65ebbd1eb4a9c23a6611
Italo Andrade <italorio@gmail.com>
2021-05-12 21:47:19 -0300

    Fix lib .

--- WhatsAppDotNet/Demo.cs
+++ WhatsAppDotNet/Demo.cs
@@ -99,6 +99,8 @@ namespace WhatsAppDotNet
 
         public async void sendMessage(object sender, EventArgs e)
         {
+
+            /*phone :   55 11 987511473  */ 
             await _ws.SendMessageMedia($"{txtPhone.Text.Trim()}@c.us" , new ContentMessage() { msg = txtMensagem.Text }, new MessageOption());
             txtMessages.AppendText(txtMensagem.Text.Trim() + "\r\n");
         }
